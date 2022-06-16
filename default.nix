cad665c616d1c67c2d7a05b461df9f12e01aade8
o1lo01ol1o <tim.pierson@gmail.com>
2022-06-16 12:57:12 +0100

    don't build HLS in ci

--- default.nix
+++ default.nix
@@ -1,20 +1,20 @@
+{ hlsSupport ? true }:
 let
-  reflex-platform = import ((import <nixpkgs> {}).fetchFromGitHub {
-    owner  = "Befunctional";
-    repo   = "reflex-platform";
-    rev    = "55a855cd0b7547a0e17fffe92650d0203f7bca6e";
+  reflex-platform = import ((import <nixpkgs> { }).fetchFromGitHub {
+    owner = "Befunctional";
+    repo = "reflex-platform";
+    rev = "55a855cd0b7547a0e17fffe92650d0203f7bca6e";
     sha256 = "0wnnfbr3nqw5q698iw5ms4dv565d4rdgiww8gvd62wwnqr0b7xrb";
-  }) {hlsSupport = true;};
-in
-reflex-platform.project ({ pkgs, ... }: {
+  }) { inherit hlsSupport; };
+in reflex-platform.project ({ pkgs, ... }: {
   packages = {
     reflex-dom-plaid = ./reflex-dom-plaid;
     demo = ./demo;
   };
 
   shells = {
-    ghc = ["reflex-dom-plaid" "demo"];
-    ghcjs = ["reflex-dom-plaid" "demo"];
+    ghc = [ "reflex-dom-plaid" "demo" ];
+    ghcjs = [ "reflex-dom-plaid" "demo" ];
   };
 })
 

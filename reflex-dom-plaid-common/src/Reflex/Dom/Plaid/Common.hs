979d4cc13a847043a679ab07f2d28d86ed150889
kirillfx <kirillfx@gmail.com>
2022-06-21 12:39:38 +0000

    fix: NFData added to deriving lists

--- reflex-dom-plaid-common/src/Reflex/Dom/Plaid/Common.hs
+++ reflex-dom-plaid-common/src/Reflex/Dom/Plaid/Common.hs
@@ -1,17 +1,19 @@
 module Reflex.Dom.Plaid.Common where
 
+import Control.DeepSeq (NFData)
+import Data.Aeson
 import Data.Hashable (Hashable)
 import Data.Text (Text)
 import GHC.Generics (Generic)
 
 newtype PlaidUserId = PlaidUserId Text
   deriving stock (Generic)
-  deriving newtype (Show, Eq, Hashable)
+  deriving newtype (Show, Eq, Hashable, NFData, ToJSON, FromJSON)
 
 newtype PlaidPublicToken = PlaidPublicToken Text
   deriving stock (Generic)
-  deriving newtype (Show, Eq)
+  deriving newtype (Show, Eq, NFData, ToJSON, FromJSON)
 
 newtype PlaidAccessToken = PlaidAccessToken Text
   deriving stock (Generic)
-  deriving newtype (Show, Eq)
+  deriving newtype (Show, Eq, NFData, ToJSON, FromJSON)

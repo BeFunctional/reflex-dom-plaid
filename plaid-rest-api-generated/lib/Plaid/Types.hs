0aa6ac9e12bcd0963d1c135df588bcb541005c53
kirillfx <kirillfx@gmail.com>
2022-07-05 14:53:42 +0000

    fix: Products

--- plaid-rest-api-generated/lib/Plaid/Types.hs
+++ plaid-rest-api-generated/lib/Plaid/Types.hs
@@ -6032,15 +6032,11 @@ newtype ProductStatusBreakdown = ProductStatusBreakdown { unProductStatusBreakdo
   deriving (Show, Eq, FromJSON, ToJSON, Generic, Data)
 
 -- | A list of products that an institution can support. All Items must be initialized with at least one product. The Balance product is always available and does not need to be specified during initialization.
-data Products = Products
-  { 
-  } deriving (Show, Eq, Generic, Data)
-
-instance FromJSON Products where
-  parseJSON = genericParseJSON (removeFieldLabelPrefix True "products")
-instance ToJSON Products where
-  toJSON = genericToJSON (removeFieldLabelPrefix False "products")
-
+newtype Products = Products
+  { unProducts :: Text
+  } 
+  deriving stock (Show, Eq, Generic, Data)
+  deriving newtype (ToJSON, FromJSON)
 
 -- | The valid name matching sensitivity configurations for a screening program. Note that while certain matching techniques may be more prevalent on less strict settings, all matching algorithms are enabled for every sensitivity.  &#x60;coarse&#x60; - See more potential matches. This sensitivity will see more broad phonetic matches across alphabets that make missing a potential hit very unlikely. This setting is noisier and will require more manual review.  &#x60;balanced&#x60; - A good default for most companies. This sensitivity is balanced to show high quality hits with reduced noise.  &#x60;strict&#x60; - Aggressive false positive reduction. This sensitivity will require names to be more similar than &#x60;coarse&#x60; and &#x60;balanced&#x60; settings, relying less on phonetics, while still accounting for character transpositions, missing tokens, and other common permutations.  &#x60;exact&#x60; - Matches must be nearly exact. This sensitivity will only show hits with exact or nearly exact name matches with only basic correction such as extraneous symbols and capitalization. This setting is generally not recommended unless you have a very specific use case.
 data ProgramNameSensitivity = ProgramNameSensitivity
